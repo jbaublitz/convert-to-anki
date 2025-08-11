9029b6ee82028bd481029043daa9501e2d9b9356
John Baublitz <john.m.baublitz@gmail.com>
2025-08-11 14:19:41 -0400

    Adapt to HTML format for Spanish entries

--- web/es.py
+++ web/es.py
@@ -10,14 +10,8 @@ def parse(html: BeautifulSoup) -> str:
     Parse HTML returned from web request for a French word.
     """
     all_tables = html.find_all("table")
-    tables = [table for table in all_tables if table.select(".lang-es") != []]
+    tables = [table for table in all_tables if table.findAll("span", {"lang": "es"}) != []]
 
-    html_str = ""
-    if tables == []:
-        adj_forms = html.select(".form-of.lang-es")
-        if adj_forms:
-            html_str += ", ".join([form.text for form in adj_forms])
-    else:
-        html_str += "<br>".join(list(map(lambda html: str(html), tables)))
+    html_str = "<br>".join(list(map(lambda html: str(html), tables)))
 
     return html_str.replace("\n", "")
